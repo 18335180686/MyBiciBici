371336abbfee39674701182ee20758e76fe0c9ed
王泽奇 <1165715057@qq.com>
2016-11-02 12:45:35 +0800

    创建项目

--- app/src/main/java/com/bicibici/mybicibici/base/BaseApplication.java
+++
@@ -1,9 +0,0 @@
-package com.bicibici.mybicibici.base;
-
-import android.app.Application;
-
-/**
- * Created by Administrator on 2016/11/2.
- */
-public class BaseApplication extends Application {
-}
