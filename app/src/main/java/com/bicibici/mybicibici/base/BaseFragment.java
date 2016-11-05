6f6d24227845c07fc591b7a6b2394b4ea5158603
liyanjun <1014222061@qq.com>
2016-11-05 11:42:43 +0800

    创建项目

---
+++ app/src/main/java/com/bicibici/mybicibici/base/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.bicibici.mybicibici.base;
+
+import android.app.Fragment;
+
+/**
+ * Created by Administrator on 2016/11/2.
+ */
+public class BaseFragment extends Fragment {
+}
---
+++ app/src/main/java/com/bicibici/mybicibici/enter/SplashActivity.java
@@ -0,0 +1,9 @@
+package com.bicibici.mybicibici.enter;
+
+import com.bicibici.mybicibici.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2016/11/2.
+ */
+public class SplashActivity extends BaseActivity {
+}
