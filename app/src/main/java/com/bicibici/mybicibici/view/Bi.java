f791120c2bb85a0761bcc7c8c1245a80c5c848de
liyanjun <1014222061@qq.com>
2016-11-05 11:46:05 +0800

    创建项目

---
+++ app/src/main/java/com/bicibici/mybicibici/view/Bi.java
@@ -0,0 +1,16 @@
+package com.bicibici.mybicibici.view;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Author LYJ
+ * Created on 2016/11/5.
+ * Time 11:38
+ */
+
+public class Bi {
+    public void show(Context context){
+        Toast.makeText(context, "HELLO", Toast.LENGTH_SHORT).show();
+    }
+}
